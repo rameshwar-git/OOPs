0bd62f4fb2acb368eb6625709ec30a56362b6184
Rameshwar Singh <112180957+rameshwar-gkp@users.noreply.github.com>
2025-04-07 15:23:30 +0530

    update

--- car.java
+++ car.java
@@ -1,5 +1,16 @@
 class car{
+  String color;
+
+  public void color(String color){
+    this.color=color;
+  }
+
+  public void display(){
+    System.out.println("Car color:"+color);
+  }
   public static void main(String[] args){
-  
+    color c=new color();
+    c.color="Red";
+    new display();
   }
 }
