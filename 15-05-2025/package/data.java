85a0733b3da087d80ac5c1d618bf3e468f0527a6
Rameshwar Singh <112180957+rameshwar-git@users.noreply.github.com>
2025-05-15 16:28:17 +0530

    Add files via upload

---
+++ 15-05-2025/package/data.java
@@ -0,0 +1,23 @@
+package mainpackeg;
+class data{
+    private String text,image,video;
+
+    public void setText(String text){
+        this.text=text;
+    }
+    public String getText(){
+        return text;
+    }
+    public void setImage(String image){
+        this.image=image;
+    }
+    public String getImage(){
+        return image;
+    }
+    public void setVideo(String video){
+        this.video=video;
+    }
+    public String getVideo(){
+        return video;
+    }
+}
---
+++ 15-05-2025/package/posts.java
@@ -0,0 +1,24 @@
+package mainpackeg;
+interface  posts{
+    abstract void text();
+    abstract void image();
+    abstract void video();
+}
+
+class facebook implements posts{
+    @Override
+    public void text() {
+
+    }
+
+    @Override
+    public void image() {
+    
+    }
+
+    @Override
+    public void video() {
+
+    }
+    
+}
